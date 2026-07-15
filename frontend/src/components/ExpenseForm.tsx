78a5881ad8252699312e2a8a96aaa2cd8c481b36
Emman Franje <emmanfranje@gmail.com>
2026-07-16 05:03:32 +0800

    feat: add option to create new expense categories

--- frontend/src/components/ExpenseForm.tsx
+++ frontend/src/components/ExpenseForm.tsx
@@ -4,9 +4,9 @@
 
 import React from "react";
 import { ExpenseFormData } from "../types";
-import { EXPENSE_CATEGORIES } from "../constants/categories";
 import { TextField, SelectBox, Button } from "../vibes";
 import { useExpenseForm } from "../hooks/useExpenseForm";
+import { useCategoryOptions } from "../hooks/useCategoryOptions";
 
 interface ExpenseFormProps {
   initialData?: Partial<ExpenseFormData>;
@@ -27,6 +27,18 @@ export function ExpenseForm({
       onSubmit,
     });
 
+  const {
+    categoryOptions,
+    isAddingCategory,
+    newCategoryName,
+    setNewCategoryName,
+    categoryError,
+    isSavingCategory,
+    startAddingCategory,
+    cancelAddingCategory,
+    saveNewCategory,
+  } = useCategoryOptions();
+
   const formStyle: React.CSSProperties = {
     display: "flex",
     flexDirection: "column",
@@ -39,10 +51,16 @@ export function ExpenseForm({
     marginTop: "0.5rem",
   };
 
-  const categoryOptions = EXPENSE_CATEGORIES.map((category) => ({
-    value: category,
-    label: category,
-  }));
+  const categoryLabelRowStyle: React.CSSProperties = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  };
+
+  const categoryLabelStyle: React.CSSProperties = {
+    fontSize: "0.875rem",
+    fontWeight: 600,
+  };
 
   return (
     <form onSubmit={handleSubmit} style={formStyle}>
@@ -69,15 +87,68 @@ export function ExpenseForm({
         required
       />
 
-      <SelectBox
-        label="Category"
-        options={categoryOptions}
-        value={formData.category}
-        onChange={(e) => handleChange("category", e.target.value)}
-        error={errors.category}
-        fullWidth
-        required
-      />
+      <div>
+        <div style={categoryLabelRowStyle}>
+          <label style={categoryLabelStyle}>
+            Category
+          </label>
+          <Button
+            type="button"
+            variant="secondary"
+            size="small"
+            onClick={startAddingCategory}
+          >
+            + Add category
+          </Button>
+        </div>
+
+        <div style={{ marginTop: "0.5rem" }}>
+          <SelectBox
+            options={categoryOptions}
+            value={formData.category}
+            onChange={(e) => handleChange("category", e.target.value)}
+            error={errors.category}
+            fullWidth
+            required
+          />
+        </div>
+
+        {isAddingCategory && (
+          <div
+            style={{
+              display: "flex",
+              gap: "0.5rem",
+              alignItems: "flex-start",
+              marginTop: "0.5rem",
+            }}
+          >
+            <TextField
+              placeholder="New category name"
+              value={newCategoryName}
+              onChange={(e) => setNewCategoryName(e.target.value)}
+              error={categoryError}
+              fullWidth
+            />
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={() =>
+                saveNewCategory((name) => handleChange("category", name))
+              }
+              disabled={isSavingCategory}
+            >
+              {isSavingCategory ? "Adding..." : "Add"}
+            </Button>
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={cancelAddingCategory}
+            >
+              Cancel
+            </Button>
+          </div>
+        )}
+      </div>
 
       <TextField
         label="Date"
